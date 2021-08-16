174d7faf106ad987e9cf01d245af924b8caa87db
fermartz <fernandomvc@gmail.com>
2021-08-16 11:47:47 -0500

    add text

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,74 @@
 import React, { useState } from "react";
-import logo from "./logo.svg";
-import Ologo from "./overchute.png";
+import Container from "@material-ui/core/Container";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import IconButton from "@material-ui/core/IconButton";
+import Twitter from "@material-ui/icons/Twitter";
+import Mail from "@material-ui/icons/Mail";
+
 import FullLogo from "./logo-full.png";
-import "./App.css";
+import Dfinity from "./dfinity.png";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <div>
-          <img src={FullLogo} className="O-logo" alt="logo" />
-
-          <p className="h2Typo">
+    <Box bgcolor="#b8b8b8">
+      <Container>
+        <Box minHeight="100vh" display="flex" flexDirection="column">
+          <img
+            src={FullLogo}
+            style={{ width: "75%", marginTop: "2rem" }}
+            alt="logo"
+          />
+          <Typography
+            variant="h4"
+            style={{
+              marginBottom: "3rem",
+              fontWeight: "700",
+              color: "#454545",
+            }}
+          >
             Crowdfunding the release of <br /> intelectual property
-          </p>
-
-          <p className="description">
-            A decentralized smart-contract application that uses <br /> a novel
-            funding protocol to mitigate the free-rider <br /> problem and
-            enable efficient price discovery.
-          </p>
-        </div>
-
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+          </Typography>
+          <Grid container>
+            <Grid item xs={10} sm={8} md={6} lg={6}>
+              <Typography variant="h6" style={{ color: "#454545" }}>
+                A decentralized smart-contract application that uses a novel
+                funding protocol to mitigate the free-rider problem and enable
+                efficient price discovery.
+              </Typography>
+            </Grid>
+          </Grid>
+          <Box mb="3rem"></Box>
+          <Grid container>
+            <Grid item xs={6} sm={6} md={6} lg={4}>
+              <IconButton
+                color="primary"
+                aria-label="upload picture"
+                component="span"
+              >
+                <Twitter fontSize="large" />
+              </IconButton>
+              <IconButton
+                color="primary"
+                aria-label="upload picture"
+                component="span"
+                style={{ marginLeft: "2rem" }}
+              >
+                <Mail fontSize="large" />
+              </IconButton>
+            </Grid>
+            <Grid item xs={6} sm={6} md={6} lg={4}>
+              <img src={Dfinity} alt="logo" style={{ marginBottom: "1rem" }} />
+              <Typography
+                variant="body1"
+                children="Build on Internet Computer"
+              />
+            </Grid>
+          </Grid>
+        </Box>
+      </Container>
+    </Box>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import {
+  createTheme,
+  responsiveFontSizes,
+  ThemeProvider,
+} from "@material-ui/core/styles";
+import App from "./App";
+
+let theme = createTheme();
+theme = responsiveFontSizes(theme);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <React.Fragment>
+      <CssBaseline />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </React.Fragment>
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
